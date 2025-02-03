50ba59ef0fa4a8a740cf666b5df97ff623e6f218
bollettino.20159 <bollettino.andrea.studente@itispaleocapa.it>
2025-02-03 12:40:27 +0100

    commit1

--- src/TestPositivo.java
+++ src/TestPositivo.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class TestPositivo {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
+        try {
+            System.out.print("inserisci numero positivo: ");
+            int input = in.nextInt();
 
+            NumeroPositivo numero = new NumeroPositivo(input);
+            System.out.println("Numero: " + numero.getNumero());
+
+        } catch (NegativeException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("ERRORE: numero inserito non valido.");
+        } finally {
+            in.close();
+        }
     }
 }
